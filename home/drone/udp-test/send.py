7e24abd4b70ad532df5ee04109a76f12ed2188f7
Michael <michael@designquest.com.hk>
2020-01-14 16:14:14 +0800

    send to local network

--- home/drone/udp-test/send.py
+++ home/drone/udp-test/send.py
@@ -2,17 +2,21 @@
 
 import socket
 import time
+import datetime
 
-UDP_IP = "127.0.0.1"
+
+UDP_IP = "192.168.192.103"
 UDP_PORT = 5005
 MESSAGE = "Hello, World!"
 
+print "Sending message"
 print "UDP target IP:", UDP_IP
 print "UDP target port:", UDP_PORT
 print "message:", MESSAGE
 
 while True:
+    now = datetime.datetime.now()
     sock = socket.socket(socket.AF_INET, # Internet
                         socket.SOCK_DGRAM) # UDP
-    sock.sendto(MESSAGE, (UDP_IP, UDP_PORT))
-    time.sleep(5)
+    sock.sendto(MESSAGE + now.strftime("%c"), (UDP_IP, UDP_PORT))
+    time.sleep(0.5)
